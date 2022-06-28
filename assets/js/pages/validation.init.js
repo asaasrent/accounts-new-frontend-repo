9673cc65620a59082d21fc87cfafd948e2a2bd5c
ismatmalik <ismatmalik0504@gmail.com>
2022-06-28 14:47:22 +0500

    assets...

---
+++ assets/js/pages/validation.init.js
@@ -0,0 +1,1 @@
+!function(){"use strict";window.addEventListener("load",function(){var t=document.getElementsByClassName("needs-validation");Array.prototype.filter.call(t,function(e){e.addEventListener("submit",function(t){!1===e.checkValidity()&&(t.preventDefault(),t.stopPropagation()),e.classList.add("was-validatedTT")},!1)})},!1)}();
